c755b4c64a81b073bddda9fc95313d133cc868e4
Kevin Petremann <k.petremann@criteo.com>
2020-09-02 17:16:17 +0200

    Fix sent/receive mismatch
    Scapy does not look to match properly packets when response is TCPerror.
    It does not look for the seq_id to match.

--- etrace/main.py
+++ etrace/main.py
@@ -18,8 +18,8 @@ class eTrace:
         self.nb_per_path = nb_per_path
         self.pretty = pretty
         self.packets = []
-        self.result = {}
         self.links = defaultdict(lambda: defaultdict(int))
+        self.result = None
 
     def fill_blanks(self, trace):
         for i in range(1, 31):
@@ -78,6 +78,41 @@ class eTrace:
 
         return {k: dict(v) for k, v in self.links.items()}
 
+    @staticmethod
+    def _rematch(ans, unans):
+        packets = {}
+        # we record "unanswered" packets
+        for pkt in unans:
+            seq = pkt[0].seq
+            if seq not in packets:
+                packets[seq] = {}
+
+            packets[seq][0] = pkt[0]
+
+        # we record sent packets
+        for pkt in ans:
+            # sequence of sent packet
+            seq = pkt[0].seq
+
+            # we store packets
+            if seq not in packets:
+                packets[seq] = {}
+            packets[seq][0] = pkt[0]
+
+        # we record received packets
+        for pkt in ans:
+            recv = pkt[1]
+            if recv.payload.name == "ICMP":
+                seq_id = recv[0]["TCPerror"].seq
+            elif recv.payload.name == "TCP":
+                seq_id = recv[0][TCP].ack - 1
+            else:
+                raise RuntimeError("Sent packet not found")
+            packets[seq_id][1] = pkt[1]
+
+        # we resort the dict by key (which is the unique identifer of the packet)
+        return {k: v for k, v in sorted(packets.items())}
+
 
     def tcpsyn_trace(self):
         """TCP syn traceroute."""
@@ -87,15 +122,16 @@ class eTrace:
 
         start = time.time()
 
+        seq_id = 1
         for src_port in range(65000, 65000 + self.nb_path):
             for ttl in range(1, 31):
                 for i in range(0, self.nb_per_path):
                     pkt = (
                         IP(dst=self.destination, ttl=ttl)
-                        / TCP(flags="S", seq=ttl, sport=src_port, dport=80)
-                        / Raw(str(src_port) + "/" + str(ttl))
+                        / TCP(flags="S", seq=seq_id, sport=src_port, dport=443)
                     )
                     self.packets.append(pkt)
+                    seq_id += 1
 
         prepared = time.time()
 
@@ -104,37 +140,36 @@ class eTrace:
             timeout=1,
             inter=0,
             verbose=0,
-            filter="(tcp and dst portrange 65000-65100) or icmp",
+            filter=f"(tcp and dst portrange 65000-{65000 + self.nb_path}) or icmp",
         )
 
-        self.result = {}
+        packets = self._rematch(ans, unans)
 
-        # need to integrate seq id to ensure matching sent with receive
-        for i in ans:
-            hop_ip = i[1][IP].src
-            sport = i[0][TCP].sport
-            ttl = i[0][IP].ttl
-            if not self.result.get(sport):
-                self.result[sport] = {}
-            if not self.result[sport].get(ttl):
-                self.result[sport][ttl] = {}
-                self.result[sport][ttl]["loss"] = 0
-                self.result[sport][ttl]["sent"] = 0
-            self.result[sport][ttl]["hop_ip"] = hop_ip
-            self.result[sport][ttl]["latency"] = i[1].time - i[0].sent_time
-            self.result[sport][ttl]["sent"] += 1
+        self.result = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
 
-        for i in unans:
-            sport = i[0][TCP].sport
-            ttl = i[0][IP].ttl
-            if not self.result.get(sport):
-                self.result[sport] = {}
-            if not self.result[sport].get(ttl):
-                self.result[sport][ttl] = {}
-                self.result[sport][ttl]["loss"] = 0
-                self.result[sport][ttl]["sent"] = 0
-            self.result[sport][ttl]["loss"] += 1
-            self.result[sport][ttl]["sent"] += 1
+        # results analysis
+        for pkt in packets.values():
+            sent_pkt = pkt[0]
+
+            # answered packet
+            if pkt.get(1):
+                recv_pkt = pkt[1]
+                hop_ip = recv_pkt[IP].src
+                sport = sent_pkt[TCP].sport
+                ttl = sent_pkt[IP].ttl
+
+                self.result[sport][ttl]["hop_ip"] = hop_ip
+                self.result[sport][ttl]["latency"] = recv_pkt.time - sent_pkt.sent_time
+                self.result[sport][ttl]["sent"] += 1
+
+            # unanswered
+            else:
+                sport = sent_pkt[TCP].sport
+                ttl = sent_pkt[IP].ttl
+                if not "hop_ip" in self.result[sport][ttl]:
+                    self.result[sport][ttl]["hop_ip"] = "..."
+                self.result[sport][ttl]["loss"] += 1
+                self.result[sport][ttl]["sent"] += 1
 
 
 if __name__ == "__main__":
@@ -164,10 +199,14 @@ if __name__ == "__main__":
         help="number of paths to test",
     )
     parser.add_argument(
-        "--pretty", action="store_true", help="pretty output",
+        "--pretty",
+        action="store_true",
+        help="pretty output",
     )
     parser.add_argument(
-        "--verbose", action="store_true", help="show all paths",
+        "--verbose",
+        action="store_true",
+        help="show all paths",
     )
 
     args = parser.parse_args()
